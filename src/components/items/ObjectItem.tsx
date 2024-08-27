b7a21f6be4ec87037cb4b62ad60611330830f15b
CrazyProger1 <crazyproger1@gmail.com>
2024-08-27 19:51:27 +0300

    fix: object image size

--- src/components/items/ObjectItem.tsx
+++ src/components/items/ObjectItem.tsx
@@ -8,7 +8,7 @@ interface ObjectItemProps {
 const ObjectItem: FC<ObjectItemProps> = ({ obj }) => {
   return (
     <div className="bg-card rounded-2xl p-4 flex flex-row">
-      <img src={obj.photo} alt="" className="rounded-2xl" width="200px" />
+      <img src={obj.photo} alt="" className="rounded-2xl size-1/3" />
       <div className="flex flex-col ml-4">
         <p className="text-copy-primary text-lg">{obj.name}</p>
         <p className="text-copy-primary text-sm">{obj.description}</p>
