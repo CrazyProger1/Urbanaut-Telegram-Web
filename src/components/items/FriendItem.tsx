6f5d370bd6a7ed8fdb2c5828598284a064ee7c6d
CrazyProger1 <crazyproger1@gmail.com>
2024-08-28 21:23:43 +0300

    fix: friend image size

--- src/components/items/FriendItem.tsx
+++ src/components/items/FriendItem.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
-import {User} from "../../types/users.ts";
-
+import { User } from "../../types/users.ts";
 
 interface FriendItemProps {
   friend: User;
@@ -9,9 +8,10 @@ interface FriendItemProps {
 const FriendItem: FC<FriendItemProps> = ({ friend }) => {
   return (
     <div className="bg-card rounded-2xl p-4 flex flex-row">
-      <img src={friend.photo} alt="" className="rounded-2xl size-2/6" />
+      <img src={friend.photo} alt="" className="rounded-2xl" width="60px" />
       <div className="flex flex-col ml-4">
         <p className="text-copy-primary text-lg">{friend.name}</p>
+        <p className="text-sm  text-copy-primary">smth</p>
       </div>
     </div>
   );
