efedee65bec1e6fae2a95d720d60891e45cba460
CrazyProger1 <crazyproger1@gmail.com>
2024-09-02 13:42:11 +0300

    fix: header paddings

--- src/components/headers/Header.tsx
+++ src/components/headers/Header.tsx
@@ -2,7 +2,7 @@ import { DEFAULT_IMAGES } from "../../constants/images.ts";
 
 const Header = () => {
   return (
-    <header className="bg-foreground rounded-b-2xl w-full p-4">
+    <header className="bg-foreground rounded-b-2xl w-full p-2">
       <img
         className="size-16 rounded-2xl"
         src={DEFAULT_IMAGES.user}
