d542d513455422e1e10c4d112fdbfdfd24de091c
CrazyProger1 <crazyproger1@gmail.com>
2024-09-03 12:31:55 +0300

    fix: tab index

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+
 
 const SettingsModal = () => {
   return (
     <div
       id="default-modal"
-      tabIndex="-1"
+
       aria-hidden="true"
       className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
