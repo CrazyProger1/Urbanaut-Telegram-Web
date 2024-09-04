d622f3b3c628b8a6effc44b89f572b74f75f84a0
CrazyProger1 <crazyproger1@gmail.com>
2024-09-04 21:38:25 +0300

    fix: footer ref

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -3,18 +3,21 @@ import { useEffect, useRef } from "react";
 import uiStore from "../../stores/UIStore.ts";
 
 const Footer = () => {
-  const footerRef = useRef(null);
+  const footerRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (footerRef.current)
+    if (footerRef.current) {
       uiStore.setFooterHeight(footerRef.current.clientHeight);
-  }, [footerRef]);
+    }
+  }, [footerRef.current]);
+
   return (
-    <footer
+    <div
       ref={footerRef}
       className="fixed inset-x-0 bottom-0 ml-4 mr-4 bg-foreground rounded-t-2xl"
     >
       <FooterNavbar />
-    </footer>
+    </div>
   );
 };
 
