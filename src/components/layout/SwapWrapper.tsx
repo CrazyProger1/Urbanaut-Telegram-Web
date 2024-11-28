7e31ea190dd2fd341d809dcc2087662f64a58357
CrazyProger1 <crazyproger1@gmail.com>
2024-11-28 18:31:52 +0200

    jajskdnm

--- src/components/layout/SwapWrapper.tsx
+++ src/components/layout/SwapWrapper.tsx
@@ -1,39 +1,6 @@
 "use client";
 
-import React, { useContext, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
-import { AnimatePresence, motion } from "framer-motion";
-import { FOOTER_PAGES } from "@/constants/nav";
-import {
-  SWIPE_CONFIDENCE_THRESHOLD,
-  SWIPE_DRAG_CONSTRAINTS,
-  SWIPE_DRAG_ELASTIC,
-  SWIPE_OFFSET,
-  SWIPE_TRANSITION,
-} from "@/constants/animations";
-import { swipePower } from "@/utils/swipe";
-import { UIContext } from "@/contexts/ui";
-
-const variants = {
-  enter: (direction: number) => {
-    return {
-      x: direction > 0 ? SWIPE_OFFSET : -SWIPE_OFFSET,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? SWIPE_OFFSET : -SWIPE_OFFSET,
-      opacity: 0,
-    };
-  },
-};
+import React from "react";
 
 interface SwapWrapperProps {
   children: React.ReactNode;
@@ -41,61 +8,8 @@ interface SwapWrapperProps {
 }
 
 const SwapWrapper = ({ children, className }: SwapWrapperProps) => {
-  const { animationsEnabled } = useContext(UIContext);
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const [[page, direction], setPage] = useState([
-    FOOTER_PAGES.includes(pathname) ? FOOTER_PAGES.indexOf(pathname) : 0,
-    0,
-  ]);
-
-  const swapPage = (direction: number) => {
-    let newPage = page + direction;
-
-    if (newPage === -1) newPage = FOOTER_PAGES.length - 1;
-    else if (newPage === FOOTER_PAGES.length) newPage = 0;
-
-    router.push(FOOTER_PAGES[newPage]);
-    setPage([newPage, direction]);
-  };
-  if (!animationsEnabled)
-    return (
-      <div className={className} key={page}>
-        {children}
-      </div>
-    );
-
-  return (
-    <div className={className}>
-      <AnimatePresence mode="wait" initial={false} custom={direction}>
-        <motion.div
-          className="flex-1 h-full flex flex-col"
-          key={page}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={SWIPE_TRANSITION}
-          drag="x"
-          dragConstraints={SWIPE_DRAG_CONSTRAINTS}
-          dragElastic={SWIPE_DRAG_ELASTIC}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x);
-
-            if (swipe < -SWIPE_CONFIDENCE_THRESHOLD) {
-              swapPage(1);
-            } else if (swipe > SWIPE_CONFIDENCE_THRESHOLD) {
-              swapPage(-1);
-            }
-          }}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-    </div>
-  );
+  return <div className={className}>{children}</div>;
+  // return <div className={className}>{children}</div>;
 };
 
 export default SwapWrapper;
--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -1,9 +1,10 @@
 export const SWIPE_CONFIDENCE_THRESHOLD = 10000;
-export const SWIPE_OFFSET = 300;
+export const SWIPE_OFFSET = 0;
 export const SWIPE_TRANSITION = {
   x: { type: "spring", stiffness: 300, damping: 30 },
-  opacity: { duration: 0.1 },
+  opacity: { duration: 0.01 },
   duration: 0.1,
+  ease: "easeIn",
 };
 export const SWIPE_DRAG_CONSTRAINTS = { left: 0, right: 0 };
-export const SWIPE_DRAG_ELASTIC = 0.1;
+export const SWIPE_DRAG_ELASTIC = 0.01;
