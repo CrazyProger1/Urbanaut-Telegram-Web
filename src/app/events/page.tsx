610418105754e432fff6030c47af43324a8427f4
CrazyProger1 <crazyproger1@gmail.com>
2024-11-28 22:17:00 +0200

    try fix: swipe wrapper

---
+++ src/app/events/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import UnavailablePage from "@/components/layout/pages/UnavailablePage";
+import { ICONS } from "@/constants/media";
+
+const EventsPage = () => <UnavailablePage icon={ICONS.lock} />;
+
+export default EventsPage;
--- src/components/layout/SwipeWrapper.tsx
+++ src/components/layout/SwipeWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
 import { FOOTER_PAGES } from "@/constants/nav";
@@ -13,6 +13,7 @@ import {
 } from "@/constants/animations";
 import { swipePower } from "@/utils/swipe";
 import { UIContext } from "@/contexts/ui";
+import { set } from "immutable";
 
 interface SwipeWrapperProps {
   children: React.ReactNode;
@@ -26,12 +27,14 @@ const SwipeWrapper = ({ children, className }: SwipeWrapperProps) => {
   const initialPage = FOOTER_PAGES.includes(pathname)
     ? FOOTER_PAGES.indexOf(pathname)
     : 0;
+  const [page, setPage] = useState(initialPage);
 
   const paginate = (direction: number) => {
     let newPage = initialPage + direction;
     if (newPage <= -1) newPage = FOOTER_PAGES.length - 1;
     else if (newPage >= FOOTER_PAGES.length) newPage = 0;
     router.push(FOOTER_PAGES[newPage]);
+    setPage(newPage);
   };
   if (!animationsEnabled) return <div className={className}>{children}</div>;
 
@@ -40,7 +43,7 @@ const SwipeWrapper = ({ children, className }: SwipeWrapperProps) => {
       <AnimatePresence mode="wait" initial={false}>
         <motion.div
           className="flex-1 h-full flex flex-col"
-          key={initialPage}
+          key={page}
           variants={SWIPE_VARIANTS}
           initial="enter"
           animate="center"
