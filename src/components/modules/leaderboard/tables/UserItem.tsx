da1f2204685ec4f96c483bfede1c16d1355d8b5d
CrazyProger1 <crazyproger1@gmail.com>
2025-01-14 16:38:33 +0200

    update: leaderboard

--- src/components/modules/leaderboard/tables/UserItem.tsx
+++ src/components/modules/leaderboard/tables/UserItem.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { User } from "@/types/users";
+import { STUBS } from "@/constants/media";
+import { LINKS } from "@/constants/nav";
+import Link from "next/link";
 
 interface Props {
   user: User;
 }
 
+const RANK_FRAME = {
+  DEFAULT: "shadow-rank-frame-newbie",
+  NEWBIE: "shadow-rank-frame-newbie",
+  STALKER: "shadow-rank-frame-stalker",
+  LEGEND: "shadow-rank-frame-legend",
+};
 const UserItem = ({ user }: Props) => {
-  return <div>{user.username}</div>;
+  return (
+    <div className="flex flex-row items-center">
+      <Link href={LINKS.profile}>
+        <img
+          className={`size-16 rounded-2xl ${RANK_FRAME.NEWBIE}`}
+          src={STUBS.user}
+          alt="user-photo"
+        />
+      </Link>
+
+      <div className="pl-2 text-text font-primary">
+        <p className="text-lg">@{user.username}</p>
+        <p className="text-text font-primary text-sm">newbie</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserItem;
