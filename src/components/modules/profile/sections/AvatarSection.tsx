cc22ade1b9082d58f19b2cca1cadaf50f0a1ca0b
CrazyProger1 <crazyproger1@gmail.com>
2025-01-14 12:31:42 +0200

    update: avatar section

--- src/components/modules/profile/sections/AvatarSection.tsx
+++ src/components/modules/profile/sections/AvatarSection.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import React from "react";
 import { STUBS } from "@/constants/media";
+import { useAccountStore } from "@/stores";
 
 const AvatarSection = () => {
+  const { initData } = useAccountStore();
   return (
     <div className="flex flex-col items-center w-full">
       <img
-        src={STUBS.user}
+        src={initData?.user?.photoUrl || STUBS.user}
         alt=""
         className="size-52 shadow-rank-frame-legend rounded-2xl"
       />
