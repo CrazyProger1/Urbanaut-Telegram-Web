9515e6dc71a7d7a2df6e4021f4ee3abb4bc91516
CrazyProger1 <crazyproger1@gmail.com>
2025-01-14 17:04:33 +0200

    add: rank clsx class

--- src/components/modules/leaderboard/tables/UserItem.tsx
+++ src/components/modules/leaderboard/tables/UserItem.tsx
@@ -3,27 +3,25 @@ import { User } from "@/types/users";
 import { STUBS } from "@/constants/media";
 import { LINKS } from "@/constants/nav";
 import Link from "next/link";
+import clsx from "clsx";
 
 interface Props {
   user: User;
 }
 
-const RANK_FRAME = {
-  DEFAULT: "shadow-rank-frame-newbie",
-  NEWBIE: "shadow-rank-frame-newbie",
-  STALKER: "shadow-rank-frame-stalker",
-  LEGEND: "shadow-rank-frame-legend",
-};
 const UserItem = ({ user }: Props) => {
   const { rank, username } = user;
+
+  const imageClassName = clsx(
+    "size-16 rounded-2xl",
+    rank === "NEWBIE" && "shadow-rank-frame-newbie",
+    rank === "STALKER" && "shadow-rank-frame-stalker",
+    rank === "LEGEND" && "shadow-rank-frame-legend",
+  );
   return (
     <div className="bg-foreground rounded-2xl p-2 pr-4 flex flex-row items-center shadow-volume-frame">
       <Link href={LINKS.profile}>
-        <img
-          className={`size-16 rounded-2xl ${RANK_FRAME[rank]}}`}
-          src={STUBS.user}
-          alt="user-photo"
-        />
+        <img className={imageClassName} src={STUBS.user} alt="user-photo" />
       </Link>
 
       <div className="pl-2 text-text font-primary">
