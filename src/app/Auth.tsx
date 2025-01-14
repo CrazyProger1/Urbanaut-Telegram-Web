bc549828a80de77854807584be4cad3c32385fe2
CrazyProger1 <crazyproger1@gmail.com>
2025-01-14 11:52:00 +0200

    try save cookies without additional request

--- src/app/Auth.tsx
+++ src/app/Auth.tsx
@@ -6,26 +6,28 @@ import { LINKS } from "@/constants/nav";
 import { useInitData } from "@/hooks/client/telegram";
 
 const Auth = () => {
-  const initData = useInitData();
+  const { initDataRaw } = useInitData();
   const router = useRouter();
 
-  const saveInitData = async () => {
-    const response = await fetch("/api/v1/auth", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(initData),
-    });
-
-    const data = await response.json();
-    console.log(data);
-  };
+  // const saveInitData = async () => {
+  //   const response = await fetch("/api/v1/auth", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(initData),
+  //   });
+  //
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
 
   useEffect(() => {
-    saveInitData().then(() => {
-      router.push(LINKS.objects);
-    });
+    // saveInitData().then(() => {
+    //   router.push(LINKS.objects);
+    // });
+    document.cookie = `initDataRaw=${initDataRaw}; path=/; max-age=3600`;
+    router.push(LINKS.objects);
   }, []);
 
   return <div>Saving initData...</div>;
