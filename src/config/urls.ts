365f7ab6bc7b4cf89ba901040946c7a661cf7655
CrazyProger1 <crazyproger1@gmail.com>
2025-01-15 23:01:04 +0200

    fix: url

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,3 +1,3 @@
 export const API_VERSION = "v1";
 export const API_URL =
-  process.env.API_URL || `https://api.urbanaut-club.com/api/${API_VERSION}`;
+  process.env.API_URL || `https://api.urbanaut.club/api/${API_VERSION}`;
