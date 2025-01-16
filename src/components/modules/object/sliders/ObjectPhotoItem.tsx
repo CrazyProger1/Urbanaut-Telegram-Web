f66ba6013c143eb88355454b9f82939a03b1fc85
CrazyProger1 <crazyproger1@gmail.com>
2025-01-16 12:51:35 +0200

    fix: object page image

--- src/components/modules/object/sliders/ObjectPhotoItem.tsx
+++ src/components/modules/object/sliders/ObjectPhotoItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AbandonedObjectPhoto } from "@/types/objects";
 import { SwiperSlide } from "swiper/react";
+import Image from "next/image";
 
 interface Props {
   photo: AbandonedObjectPhoto;
@@ -8,10 +9,13 @@ interface Props {
 
 const ObjectPhotoItem = ({ photo }: Props) => (
   <SwiperSlide key={photo.src}>
-    <img
+    <Image
       className="object-cover max-h-80 rounded-2xl"
       src={photo.src}
       alt="object-photo"
+      placeholder="blur"
+      width={420}
+      height={320}
     />
   </SwiperSlide>
 );
