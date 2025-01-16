0157862ede89c4ec1f5771c8175cd45c5bb4b721
CrazyProger1 <crazyproger1@gmail.com>
2025-01-16 17:45:08 +0200

    fix: probably ol & ul colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config = {
   plugins: [require("@tailwindcss/typography")],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            ul: {
+              color: "rgba(var(--color-mdx-ul))",
+            },
+            ol: {
+              color: "rgba(var(--color-mdx-ol))",
+            },
+          },
+        },
+      },
       container: {
         center: true,
         padding: {
