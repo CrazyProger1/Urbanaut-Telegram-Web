2b74329357bcfc2b73633217602fa70f98a93532
CrazyProger1 <crazyproger1@gmail.com>
2025-01-17 21:10:08 +0200

    fix: stats

--- src/components/modules/object/tabs/StatsTab.tsx
+++ src/components/modules/object/tabs/StatsTab.tsx
@@ -18,15 +18,17 @@ const StatsTab = ({ object }: Props) => {
   } = object;
   return (
     <div className="flex flex-col font-primary text-text">
-      <div className="flex flex-col bg-foreground w-full p-4">
+      <div className="flex flex-col bg-foreground w-full p-4 rounded-2xl">
         <div className="font-bold text-xl text-center">Dates</div>
+        <div className="mt-2" />
         <p>Created at: {formatDate(created_at)}</p>
         <p>Abandoned at: {formatDate(abandoned_at)}</p>
         <p>Abandoned at: {formatDate(built_at)}</p>
       </div>
       <div className="mt-4" />
-      <div className="flex flex-col bg-foreground w-full p-4">
+      <div className="flex flex-col bg-foreground w-full p-4 rounded-2xl">
         <div className="font-bold text-xl text-center">State</div>
+        <div className="mt-2" />
         <p>Preservation level: {preservation_level}</p>
         <p>Security level: {security_level}</p>
         <p>Difficulty at: {difficulty_level}</p>
