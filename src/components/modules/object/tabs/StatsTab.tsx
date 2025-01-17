9a26ac8606db4633d0dcdeb8c30554cd80728e83
CrazyProger1 <crazyproger1@gmail.com>
2025-01-17 21:00:17 +0200

    add: states

--- src/components/modules/object/tabs/StatsTab.tsx
+++ src/components/modules/object/tabs/StatsTab.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import { AbandonedObject } from "@/types/objects";
+import { RatingBar } from "@/components/common/bars/ratings";
 
 interface Props {
   object: AbandonedObject;
 }
 
 const StatsTab = ({ object }: Props) => {
-  return <div>{object.name}</div>;
+  const {
+    created_at,
+    abandoned_at,
+    built_at,
+    preservation_level,
+    security_level,
+    difficulty_level,
+  } = object;
+  return (
+    <div className="flex flex-col font-primary text-text">
+      <div className="flex flex-col bg-foreground w-full p-4">
+        <div className="font-bold text-xl">Dates</div>
+        <p>Created at: {String(created_at)}</p>
+        <p>Abandoned at: {String(abandoned_at)}</p>
+        <p>Abandoned at: {String(built_at)}</p>
+      </div>
+      <div className="mt-4" />
+      <div className="flex flex-col bg-foreground w-full p-4">
+        <div className="font-bold text-xl">State</div>
+        <p>Preservation level: {preservation_level}</p>
+        <p>Security level: {security_level}</p>
+        <p>Difficulty at: {difficulty_level}</p>
+      </div>
+      <div className="mt-4" />
+      <RatingBar stars={3} />
+    </div>
+  );
 };
 
 export default StatsTab;
