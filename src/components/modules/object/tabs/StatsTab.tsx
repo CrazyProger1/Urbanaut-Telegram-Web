fbab66fc8ec849c112907b743eaf5ae350af53e8
CrazyProger1 <crazyproger1@gmail.com>
2025-01-17 21:18:51 +0200

    fix: stats

--- src/components/modules/object/tabs/StatsTab.tsx
+++ src/components/modules/object/tabs/StatsTab.tsx
@@ -21,35 +21,35 @@ const StatsTab = ({ object }: Props) => {
       <div className="flex flex-col bg-foreground w-full p-4 rounded-2xl">
         <div className="font-bold text-xl text-center">Dates</div>
         <div className="mt-2" />
-        <p>
+        <div>
           <div className="font-bold">Created at:</div>
           {formatDate(created_at)}
-        </p>
-        <p>
+        </div>
+        <div>
           <div className="font-bold">Abandoned at:</div>
           {formatDate(abandoned_at)}
-        </p>
-        <p>
+        </div>
+        <div>
           <div className="font-bold">Abandoned at:</div>
           {formatDate(built_at)}
-        </p>
+        </div>
       </div>
       <div className="mt-4" />
       <div className="flex flex-col bg-foreground w-full p-4 rounded-2xl">
         <div className="font-bold text-xl text-center">State</div>
         <div className="mt-2" />
-        <p>
+        <div>
           <div className="font-bold">Preservation level:</div>
           {preservation_level}
-        </p>
-        <p>
+        </div>
+        <div>
           <div className="font-bold">Security level:</div>
           {security_level}
-        </p>
-        <p>
+        </div>
+        <div>
           <div className="font-bold">Difficulty at:</div>
           {difficulty_level}
-        </p>
+        </div>
       </div>
       <div className="mt-4" />
       <RatingBar stars={3} />
