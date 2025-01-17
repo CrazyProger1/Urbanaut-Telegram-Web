5e079edb66863a5041e5767859e7c633ca28acb1
CrazyProger1 <crazyproger1@gmail.com>
2025-01-17 22:04:27 +0200

    add: heading centering

--- src/components/modules/object/tabs/DescriptionTab.tsx
+++ src/components/modules/object/tabs/DescriptionTab.tsx
@@ -12,12 +12,12 @@ const DescriptionTab = ({ object }: Props) => {
       className="
       text-text font-primary w-full prose 
       prose-strong:font-primary
-      prose-h1:text-text prose-h1:text-2xl prose-h1:font-primary
-      prose-h2:text-text prose-h2:text-xl prose-h2:font-primary
-      prose-h3:text-text prose-h3:text-lg prose-h3:font-primary
-      prose-h4:text-text prose-h4:text-md prose-h4:font-primary
-      prose-h5:text-text prose-h5:text-sm prose-h5:font-primary
-      prose-h6:text-text prose-h6:text-xs prose-h6:font-primary
+      prose-h1:text-text prose-h1:text-2xl prose-h1:font-primary prose-h1:text-center
+      prose-h2:text-text prose-h2:text-xl prose-h2:font-primary prose-h2:text-center
+      prose-h3:text-text prose-h3:text-lg prose-h3:font-primary prose-h3:text-center
+      prose-h4:text-text prose-h4:text-md prose-h4:font-primary prose-h4:text-center
+      prose-h5:text-text prose-h5:text-sm prose-h5:font-primary prose-h5:text-center
+      prose-h6:text-text prose-h6:text-xs prose-h6:font-primary prose-h6:text-center
     
       prose-hr:border-text
       max-w-full 
