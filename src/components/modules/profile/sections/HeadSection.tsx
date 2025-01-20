6187d23c8466e6de8ad297d18045d33acf1a7454
CrazyProger1 <crazyproger1@gmail.com>
2025-01-20 21:05:44 +0200

    username

--- src/components/modules/profile/sections/HeadSection.tsx
+++ src/components/modules/profile/sections/HeadSection.tsx
@@ -3,9 +3,10 @@ import { ICONS } from "@/constants/media";
 
 const HeadSection = () => {
   return (
-    <div className="flex">
+    <div className="flex flex-col gap-4">
       <div className="flex-1" />
       <img src={ICONS.bell} alt="" className="size-8" />
+      <div className="font-primary font-bold text-text text-xl">@username</div>
     </div>
   );
 };
