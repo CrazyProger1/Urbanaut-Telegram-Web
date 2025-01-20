eab85d3d5fb46b5b3a4dd820bdb24f25ea8310d9
CrazyProger1 <crazyproger1@gmail.com>
2025-01-20 21:11:50 +0200

    fix: avatar section

--- src/components/modules/profile/sections/AvatarSection.tsx
+++ src/components/modules/profile/sections/AvatarSection.tsx
@@ -8,6 +8,10 @@ const AvatarSection = () => {
   const { initData } = useAccountStore();
   return (
     <div className="flex flex-col items-center w-full">
+      <div className="font-primary font-bold text-text text-xl text-center">
+        @username
+      </div>
+      <div className="mt-4" />
       <img
         src={initData?.user?.photoUrl || STUBS.user}
         alt=""
--- src/components/modules/profile/sections/HeadSection.tsx
+++ src/components/modules/profile/sections/HeadSection.tsx
@@ -3,10 +3,9 @@ import { ICONS } from "@/constants/media";
 
 const HeadSection = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-row">
       <div className="flex-1" />
       <img src={ICONS.bell} alt="" className="size-8" />
-      <div className="font-primary font-bold text-text text-xl">@username</div>
     </div>
   );
 };
