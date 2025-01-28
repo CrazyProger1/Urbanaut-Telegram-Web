38fde86b7977efdd77edf38a0f4b57945725c229
CrazyProger1 <crazyproger1@gmail.com>
2025-01-28 21:10:12 +0200

    fix: search form?

--- src/components/modules/objects/forms/SearchForm.tsx
+++ src/components/modules/objects/forms/SearchForm.tsx
@@ -1,15 +1,29 @@
 import React from "react";
-import Form from "next/form";
+import Form from "next/server";
 import { SearchTextInput } from "@/components/common/contols/inputs";
-import { LINKS } from "@/constants/nav";
+import { redirect } from "@/i18n/routing";
+import { getLocale } from "next-intl/server";
 
 interface Props {
   className?: string;
 }
 
+const search = async (formData: FormData) => {
+  "use server";
+
+  const query = formData.get("query")?.toString() || "";
+
+  const locale = await getLocale();
+
+  redirect({
+    href: `/objects?query=${encodeURIComponent(query)}`,
+    locale,
+  });
+};
+
 const SearchForm = ({ className }: Props) => {
   return (
-    <Form className="w-full" action={LINKS.objects}>
+    <Form className="w-full" action={search}>
       <SearchTextInput className={className} />
     </Form>
   );
