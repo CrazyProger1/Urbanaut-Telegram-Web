80cecfa521271bc61481869d38fdca64a3f441a4
CrazyProger1 <crazyproger1@gmail.com>
2025-01-28 21:43:48 +0200

    fix: search form?

--- src/components/modules/objects/forms/SearchForm.tsx
+++ src/components/modules/objects/forms/SearchForm.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { SearchTextInput } from "@/components/common/contols/inputs";
-import { searchAction } from "@/components/modules/objects/forms/actions";
+import { LINKS } from "@/constants/nav";
+import { getLocale } from "next-intl/server";
 
 interface Props {
   className?: string;
 }
 
 const SearchForm = ({ className }: Props) => {
+  const locale = getLocale();
+  const localizedURL = `${locale}/${LINKS.objects}`;
   return (
-    <form className="w-full" action={searchAction}>
+    <form className="w-full" action={localizedURL} method="GET">
       <SearchTextInput name="query" className={className} />
       <button type="submit" style={{ display: "none" }} />
     </form>
