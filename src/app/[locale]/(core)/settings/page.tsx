00d2fe00660b7d7be05059e137764f946c5de84c
CrazyProger1 <crazyproger1@gmail.com>
2025-04-15 22:06:27 +0300

    add: init data printing

--- src/app/[locale]/(core)/settings/page.tsx
+++ src/app/[locale]/(core)/settings/page.tsx
@@ -1,2 +1,11 @@
-const SettingsPage = () => <div></div>;
+import { useLaunchParams } from "@telegram-apps/sdk-react";
+
+const SettingsPage = () => {
+  const params = useLaunchParams();
+  const initDataRaw = params.initDataRaw;
+
+  return (
+    <div className="cursor-copy text-text font-primary">{initDataRaw}</div>
+  );
+};
 export default SettingsPage;
