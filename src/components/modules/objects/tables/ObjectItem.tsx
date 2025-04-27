c93ddba56ac6aa7b58f70dec7dd0ba40b36f81c2
CrazyProger1 <crazyproger1@gmail.com>
2025-04-27 19:11:45 +0300

    fix: ObjectItem

--- src/components/modules/objects/tables/ObjectItem.tsx
+++ src/components/modules/objects/tables/ObjectItem.tsx
@@ -10,19 +10,19 @@ interface Props {
 
 const ObjectItem = ({ object }: Props) => {
   return (
-    <Block className="flex flex-row">
+    <Block className="flex flex-row p-2">
       <Image
-        className="aspect-square size-32"
+        className="aspect-square size-32 rounded-2xl"
         width={64}
         height={64}
         src={object.photo || STUBS.OBJECT}
         alt={ALTS.OBJECT_PHOTO}
       />
-      <div className="ml-4 flex flex-col">
-        <div>{object.name}</div>
-        <div>{object.short_description}</div>
+      <div className="ml-4 flex flex-col font-primary">
+        <div className="text-lg font-bold">{object.name}</div>
+        <div className="text-sm">{object.short_description}</div>
         <div></div>
-        <div></div>
+        <div>{object.rating}</div>
       </div>
     </Block>
   );
