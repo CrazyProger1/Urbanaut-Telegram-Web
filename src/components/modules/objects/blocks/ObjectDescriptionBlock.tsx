0b319a76c80b05a5f3fe445ad681675a959b245b
CrazyProger1 <crazyproger1@gmail.com>
2025-04-28 12:02:42 +0300

    add: mdx rendering

--- src/components/modules/objects/blocks/ObjectDescriptionBlock.tsx
+++ src/components/modules/objects/blocks/ObjectDescriptionBlock.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AbandonedObject } from "@/types/abandoned";
 import { Block } from "@/components/common/blocks";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 interface Props {
   object: AbandonedObject;
@@ -48,7 +49,28 @@ const ObjectDescriptionBlock = ({ object }: Props) => {
           text={section.title}
           query={`des${index}cription`}
         >
-          <div className="p-2 whitespace-pre-line">{section.content}</div>
+          <div className="p-2">
+            <div
+              className="
+      text-text font-primary w-full prose text-xs
+      prose-strong:font-primary
+      prose-h1:text-text prose-h1:text-2xl prose-h1:font-primary prose-h1:text-center
+      prose-h2:text-text prose-h2:text-xl prose-h2:font-primary prose-h2:text-center
+      prose-h3:text-text prose-h3:text-lg prose-h3:font-primary prose-h3:text-center
+      prose-h4:text-text prose-h4:text-md prose-h4:font-primary prose-h4:text-center
+      prose-h5:text-text prose-h5:text-sm prose-h5:font-primary prose-h5:text-center
+      prose-h6:text-text prose-h6:text-xs prose-h6:font-primary prose-h6:text-center
+
+      prose-hr:border-text
+      max-w-full
+      prose-strong:text-text
+      prose-ul:text-mdx-ul
+      prose-ol:text-mdx-ol
+      prose-a:text-mdx-a"
+            >
+              <MDXRemote source={section.content as string} />
+            </div>
+          </div>
         </Block.Expand>
       ))}
     </Block>
